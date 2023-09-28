f831a39b219a0620e021b33b802fdb71c4ec9ea5
kelvin-thegreat <oumaklvn2018@gmail.com>
2023-09-28 05:39:04 -0700

    Interview Trivia

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,19 +1,18 @@
 #!/usr/bin/python3
-"""
-Pascal's Triangle
-"""
+'''A module for working with Pascal's triangle.'''
+
 
 def pascal_triangle(n):
-    """
-    Generate Pascal's triangle up to the nth row and return it as a list of lists.
-    """
-    result = []
-    if n > 0:
-        for i in range(1, n + 1):
-            row = []
-            coefficient = 1
-            for j in range(1, i + 1):
-                row.append(coefficient)
-                coefficient = coefficient * (i - j) // j
-            result.append(row)
-    return result
+    '''Creates a list of lists of integers representing'''
+    tri = []
+    if type(n) is not int or n <= 0:
+        return tri
+    for i in range(n):
+        line = []
+        for j in range(i + 1):
+            if j == 0 or j == i:
+                line.append(1)
+            elif i > 0 and j > 0:
+                line.append(tri[i - 1][j - 1] + tri[i - 1][j])
+        tri.append(line)
+    return tri
