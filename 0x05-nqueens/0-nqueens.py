92e996454da85ed5de3bb975af46c37afc1f1e08
SE. kelvin <71261868+kelvin-thegreat@users.noreply.github.com>
2023-11-02 18:23:08 +0300

    Update 0-nqueens.py

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -21,19 +21,19 @@ def get_board_size():
     global board_size
     board_size = 0
 
-    """ Check if the correct number of arguments is provided. """
+    # Check if the correct number of arguments is provided.
     if len(sys.argv) != 2:
         print("Usage: nqueens N")
         sys.exit(1)
 
-    """ Attempt to convert the argument to an integer. """
+    # Attempt to convert the argument to an integer.
     try:
         board_size = int(sys.argv[1])
     except Exception:
         print("N must be a number")
         sys.exit(1)
 
-    """ Ensure that the chessboard size is at least 4. """
+    # Ensure that the chessboard size is at least 4.
     if board_size < 4:
         print("N must be at least 4")
         sys.exit(1)
@@ -50,7 +50,7 @@ def is_attacking(position1, position2):
     Returns:
         bool: True if the queens are in an attacking position else False.
     """
-    return (position1[0] == position2[0]) or (position1[1] == position2[1]) or (abs(position1[0] - position2[0]) == abs(position1[1] - position2[1])
+    return (position1[0] == position2[0]) or (position1[1] == position2[1]) or (abs(position1[0] - position2[0]) == abs(position1[1] - position2[1))
 
 def group_exists(existing_group):
     """Checks if a group exists in the solutions.
@@ -63,7 +63,7 @@ def group_exists(existing_group):
     """
     global queen_solutions
 
-    """ Check if the given group of positions exists in the solutions. """
+    # Check if the given group of positions exists in the solutions.
     return any(all(solution_pos[0] == group_pos[0] and solution_pos[1] == group_pos[1] for solution_pos in solution) for solution in queen_solutions)
 
 def build_solution(row, group):
@@ -79,17 +79,17 @@ def build_solution(row, group):
     if row == board_size:
         temporary_group = group.copy()
 
-        """ Add the solution to the list if it does not already exist. """
+        # Add the solution to the list if it does not already exist.
         if not group_exists(temporary_group):
             queen_solutions.append(temporary_group)
     else:
         for col in range(board_size):
             a = (row * board_size) + col
-            matches = zip([queen_positions[a]] * len(group), group)
+            matches = list(zip([queen_positions[a]] * len(group), group))
             used_positions = map(lambda x: is_attacking(x[0], x[1]), matches)
             group.append(queen_positions[a].copy())
 
-            """ Recursively build the solution if no attacking positions are found. """
+            # Recursively build the solution if no attacking positions are found.
             if not any(used_positions):
                 build_solution(row + 1, group)
             group.pop()
@@ -106,6 +106,6 @@ def find_solutions():
 board_size = get_board_size()
 find_solutions()
 
-""" Print the solutions. """
+# Print the solutions.
 for solution in queen_solutions:
     print(solution)
