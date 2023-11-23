7ef97fe91d6dd53be081f37c9b18fdb6bfd09fa8
SE. kelvin <71261868+kelvin-thegreat@users.noreply.github.com>
2023-11-23 08:43:05 +0300

    Task 0. Changes Comes From Within

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -2,22 +2,35 @@
 """
 Module for making change using the fewest number of coins
 """
+def make_change(coins, total):
+    """
+    Determines the fewest number of coins needed to meet a given
+    amount total when given a pile of coins of different values.
 
-def makeChange(coins, total):
+    Args:
+        coins (list): List of coin values.
+        total (int): The target total amount.
+
+    Returns:
+        int: The fewest number of coins needed, or -1 if not possible.
+    """
     if total <= 0:
         return 0
 
-    # Initialize a table to store the minimum number of coins for each amount
-    dp = [float('inf')] * (total + 1)
+    remaining_amount = total
+    coins_count = 0
+    coin_index = 0
+    sorted_coins = sorted(coins, reverse=True)
+    num_coins = len(coins)
 
-    # Base case: 0 coins needed to make change for amount 0
-    dp[0] = 0
+    while remaining_amount > 0:
+        if coin_index >= num_coins:
+            return -1
 
-    # Iterate through each coin value
-    for coin in coins:
-        # Update the table for each possible amount
-        for amount in range(coin, total + 1):
-            dp[amount] = min(dp[amount], dp[amount - coin] + 1)
+        if remaining_amount - sorted_coins[coin_index] >= 0:
+            remaining_amount -= sorted_coins[coin_index]
+            coins_count += 1
+        else:
+            coin_index += 1
 
-    # If dp[total] is still infinity, it means the total cannot be achieved
-    return dp[total] if dp[total] != float('inf') else -1
+    return coins_count
