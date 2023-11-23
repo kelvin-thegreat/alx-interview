02b95b3ef19c0461690dca17eb97c9158daa791e
kelvin-thegreat <oumaklvn2018@gmail.com>
2023-11-22 21:38:12 -0800

    Task 0. Changes Comes from Within

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+Module for making change using the fewest number of coins
+"""
+
+def makeChange(coins, total):
+    if total <= 0:
+        return 0
+
+    # Initialize a table to store the minimum number of coins for each amount
+    dp = [float('inf')] * (total + 1)
+
+    # Base case: 0 coins needed to make change for amount 0
+    dp[0] = 0
+
+    # Iterate through each coin value
+    for coin in coins:
+        # Update the table for each possible amount
+        for amount in range(coin, total + 1):
+            dp[amount] = min(dp[amount], dp[amount - coin] + 1)
+
+    # If dp[total] is still infinity, it means the total cannot be achieved
+    return dp[total] if dp[total] != float('inf') else -1
