fd5896ba5d8b8d653105da437d6df6194697ed1d
kelvin-thegreat <oumaklvn2018@gmail.com>
2023-11-28 01:18:57 -0800

    Task 0. Island Perimeter

---
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""
+Module for island_perimeter function.
+"""
+
+
+def island_perimeter(grid):
+    """
+    Calculates the perimeter of the island described in grid.
+
+    Args:
+        grid (list): List of list of integers representing the island.
+
+    Returns:
+        int: Perimeter of the island.
+    """
+    if not grid or not grid[0]:
+        return 0
+
+    perimeter = 0
+    rows, cols = len(grid), len(grid[0])
+
+    for i in range(rows):
+        for j in range(cols):
+            if grid[i][j] == 1:
+                perimeter += 4
+                if i > 0 and grid[i - 1][j] == 1:
+                    perimeter -= 2
+                if j > 0 and grid[i][j - 1] == 1:
+                    perimeter -= 2
+
+    return perimeter
+
