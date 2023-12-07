fa94a30b1fa154c4c830593d6038e31b3cd93d99
SE. kelvin <71261868+kelvin-thegreat@users.noreply.github.com>
2023-12-08 00:41:09 +0300

    Task 0. Prime Game

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -1,13 +1,26 @@
 #!/usr/bin/python3
-"""Prime Game Module"""
-
+"""Prime game module.
+"""
 
 def isWinner(x, nums):
-    """Determines the winner of each round and overall winner."""
+    """Determines the winner of a prime game session with `x` rounds.
+
+    Args:
+        x (int): The number of rounds in the game.
+        nums (List[int]): An array of integers representing the range for each round.
+
+    Returns:
+        str or None: The name of the player that won the most rounds (either 'Maria' or 'Ben').
+                     If the winner cannot be determined, returns None.
+
+    Note:
+        The function assumes that Maria always goes first and both players play optimally.
+        It also ensures that the provided inputs adhere to the specified constraints.
+    """
     if x < 1 or not nums:
         return None
     marias_wins, bens_wins = 0, 0
-    """ generate primes with a limit of the maximum number in nums"""
+    # generate primes with a limit of the maximum number in nums
     n = max(nums)
     primes = [True for _ in range(1, n + 1, 1)]
     primes[0] = False
@@ -16,7 +29,7 @@ def isWinner(x, nums):
             continue
         for j in range(i + i, n + 1, i):
             primes[j - 1] = False
-    """ filtering the number of primes less than n in nums for each round"""
+    # filter the number of primes less than n in nums for each round
     for _, n in zip(range(x), nums):
         primes_count = len(list(filter(lambda x: x, primes[0: n])))
         bens_wins += primes_count % 2 == 0
